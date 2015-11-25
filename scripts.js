cd89148dbd51516bd3360c71a649a7159b18d599
Justin Hernandez <jmarthernandez@gmail.com>
2015-11-24 20:14:32 -0600

    Add color squares for results

--- scripts.js
+++ scripts.js
@@ -3,11 +3,11 @@
   var colors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"].map(function (n) {return n.toLowerCase()});
   var $input = document.querySelector("#bg-color-input");
   var $container = document.querySelector("#bg-color-container");
-  var $results = document.querySelector("#ac-list");
+  var $results = document.querySelector("div#ac-list");
   var $node, change;
 
-  function setBackground (text) {
-    this.style.backgroundColor = text;
+  function setBackground (color) {
+    this.style.backgroundColor = color;
   }
   
   function clearColors () {
@@ -19,15 +19,16 @@
   function searchColors (substr) {
     colors.forEach(function (color) {
       if( color.indexOf(substr.toLowerCase()) >= 0) {
-        var el = document.createElement('li');
+        var el = document.createElement('div');
         el.innerHTML = color;
-        el.className = 'color-node'
+        el.style.backgroundColor = color;
+        el.className = 'color-node col-md-1'
         $results.appendChild(el)
       }
     })
 
 
-    $node = document.querySelectorAll("#ac-list li");
+    $node = document.querySelectorAll(".color-node");
     
     change = Rx.Observable.fromEvent($node, 'click')
       .map(function (e) { return e.target.innerHTML });
