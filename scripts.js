dd30b2126ef7dc0cbdeecf454f916cbfec892348
Justin Hernandez <jmarthernandez@gmail.com>
2015-11-26 13:01:00 -0600

    refactor autocomplete

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 //bg-color
 (function (){
-  var colors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"].map(function (n) {return n.toLowerCase()});
+  var Observable = Rx.Observable; 
+  var colors = ["AliceBlue","AntiqueWhite","Aqua","Aquamarine","Azure","Beige","Bisque","Black","BlanchedAlmond","Blue","BlueViolet","Brown","BurlyWood","CadetBlue","Chartreuse","Chocolate","Coral","CornflowerBlue","Cornsilk","Crimson","Cyan","DarkBlue","DarkCyan","DarkGoldenRod","DarkGray","DarkGrey","DarkGreen","DarkKhaki","DarkMagenta","DarkOliveGreen","Darkorange","DarkOrchid","DarkRed","DarkSalmon","DarkSeaGreen","DarkSlateBlue","DarkSlateGray","DarkSlateGrey","DarkTurquoise","DarkViolet","DeepPink","DeepSkyBlue","DimGray","DimGrey","DodgerBlue","FireBrick","FloralWhite","ForestGreen","Fuchsia","Gainsboro","GhostWhite","Gold","GoldenRod","Gray","Grey","Green","GreenYellow","HoneyDew","HotPink","IndianRed","Indigo","Ivory","Khaki","Lavender","LavenderBlush","LawnGreen","LemonChiffon","LightBlue","LightCoral","LightCyan","LightGoldenRodYellow","LightGray","LightGrey","LightGreen","LightPink","LightSalmon","LightSeaGreen","LightSkyBlue","LightSlateGray","LightSlateGrey","LightSteelBlue","LightYellow","Lime","LimeGreen","Linen","Magenta","Maroon","MediumAquaMarine","MediumBlue","MediumOrchid","MediumPurple","MediumSeaGreen","MediumSlateBlue","MediumSpringGreen","MediumTurquoise","MediumVioletRed","MidnightBlue","MintCream","MistyRose","Moccasin","NavajoWhite","Navy","OldLace","Olive","OliveDrab","Orange","OrangeRed","Orchid","PaleGoldenRod","PaleGreen","PaleTurquoise","PaleVioletRed","PapayaWhip","PeachPuff","Peru","Pink","Plum","PowderBlue","Purple","Red","RosyBrown","RoyalBlue","SaddleBrown","Salmon","SandyBrown","SeaGreen","SeaShell","Sienna","Silver","SkyBlue","SlateBlue","SlateGray","SlateGrey","Snow","SpringGreen","SteelBlue","Tan","Teal","Thistle","Tomato","Turquoise","Violet","Wheat","White","WhiteSmoke","Yellow","YellowGreen"].map(function (n) {return n.toLowerCase();});
   var $input = document.querySelector("#bg-color-input");
   var $container = document.querySelector("#bg-color-container");
   var $results = document.querySelector("div#ac-list");
   var $clear = document.querySelector("#bg-color-clear");
-  var $node, change;
+  var $node, change, search, clear;
 
   function setBackground (color) {
     this.style.backgroundColor = color;
@@ -19,35 +20,34 @@
 
   function searchColors (substr) {
     colors.forEach(function (color) {
-      if( color.indexOf(substr.toLowerCase()) >= 0) {
+      if(color.indexOf(substr.toLowerCase()) > -1) {
         var el = document.createElement('div');
         el.innerHTML = color;
         el.style.backgroundColor = color;
-        el.className = 'color-node col-md-1'
-        $results.appendChild(el)
+        el.className = 'color-node col-md-1';
+        $results.appendChild(el);
       }
-    })
+    });
 
     $node = document.querySelectorAll(".color-node");
-    
-    change = Rx.Observable.fromEvent($node, 'click')
-      .map(function (e) { return e.target.innerHTML });
+
+    change = Observable.fromEvent($node, 'click').
+      map(function (e) { return e.target.innerHTML; });
 
     change.subscribe(setBackground.bind($container));
   }
 
-  var search = Rx.Observable.fromEvent($input, 'keyup')
-    .map(function (e) { return e.target.value })
+  search = Observable.fromEvent($input, 'keyup').
+    throttle(100).
+    map(function (key) { return $input.value; }).
+    distinctUntilChanged().
+    map(function (search) { clearColors.call($results); return search; });
 
-  var reset = Rx.Observable.fromEvent($input, 'keydown')
-    .map(function (e) { return e.target.value })
+  clear = Observable.fromEvent($clear, 'click')
+    .map(function (e) { return e; });
 
-  var clear = Rx.Observable.fromEvent($clear, 'click')
-    .map(function (e) { return e })
-
-  search.subscribe(searchColors.bind($results))
-  reset.subscribe(clearColors.bind($results))
-  clear.subscribe(clearColors.bind($results))
+  search.subscribe(searchColors);
+  clear.subscribe(clearColors.bind($results));
 }());
 
 //change-size
@@ -64,24 +64,25 @@
   source.subscribe(setSize.bind($text))
 }());
 
+//swipe
 (function (){
   $swipe = document.querySelector("#swipe-inner");
   $outer = document.querySelector("#swipe-outer");
-  console.log($swipe)
-  console.log($outer)
 
   var swipeMouseDowns = Rx.Observable.fromEvent($swipe, "mousedown"),
   outerMouseMoves = Rx.Observable.fromEvent($outer, "mousemove"),
   outerMouseUps = Rx.Observable.fromEvent($outer, "mouseup"),
   swipeMouseUps = Rx.Observable.fromEvent($swipe, "mouseup"),
 
-  swipeMouseDrags = swipeMouseDowns.concatMap(function (down) {
+  swipeMouseDrags = swipeMouseDowns.concatMap(function (contactPoint) {
     return outerMouseMoves.takeUntil(outerMouseUps)
+      .map(function(movePoint) {
+        return movePoint.pageX - contactPoint.offsetX
+      });
   })
 
   function move (dragPoint) {
-    console.log(dragPoint)
-    this.style.left = dragPoint.pageX -50 + "px";
+    this.style.left = dragPoint + "px";
   }
 
   swipeMouseDrags.subscribe(move.bind($swipe));
